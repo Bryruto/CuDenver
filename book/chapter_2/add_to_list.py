1f8b78c8de8ebf11de612fd3c6d47212c2b88d4a
Bryruto <114258995+Bryruto@users.noreply.github.com>
2025-08-29 17:05:44 -0600

    8/29/25

---
+++ book/chapter_2/add_to_list.py
@@ -0,0 +1,17 @@
+family = ["brycen","kylie","caden","taylor","diana","billy","aaron","enis","natilie"]
+
+print(family)
+
+#insert to add a index
+family.insert(0,"start in index 0 with insert")
+
+print(family)
+
+family.insert(len(family)//2 , "middle index with insert")
+
+print(family)
+
+#use append to add to the end of a list 
+family.append("end used append")
+
+print(family)
--- book/chapter_2/function.py
+++ book/chapter_2/function.py
@@ -1,1 +1,10 @@
-list
+family = ["brycen","kylie","caden","taylor","diana","billy","aaron","enis","natilie"]
+
+print(f"{len(family)} people in my family")
+
+print(f"if {family[0]} died i wood have {len(family) - 1}")
+
+family.remove("brycen")# only deletes the frist occurence of the value but it is lost after you remove 
+#also can use pop or del
+#dead = family.pop(0) with the index with pop you make a variable with the person stored there if pop() its the last index 
+#del family[0] with the index just removes and you cant get it back
