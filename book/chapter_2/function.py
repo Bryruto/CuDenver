6db040e9dbfbc78e37b3705a8a82c95da05bb2f9
Bryruto <114258995+Bryruto@users.noreply.github.com>
2025-08-30 17:03:35 -0600

    update 8/30

--- book/chapter_2/function.py
+++ book/chapter_2/function.py
@@ -1,10 +1,19 @@
 family = ["brycen","kylie","caden","taylor","diana","billy","aaron","enis","natilie"]
 
+print(f"{family} original family")
 print(f"{len(family)} people in my family")
 
-print(f"if {family[0]} died i wood have {len(family) - 1}")
+print(f"if {family[0]} died i would have {len(family) - 1}")
 
 family.remove("brycen")# only deletes the frist occurence of the value but it is lost after you remove 
 #also can use pop or del
 #dead = family.pop(0) with the index with pop you make a variable with the person stored there if pop() its the last index 
 #del family[0] with the index just removes and you cant get it back
+print(f"{family} removed brycen")
+print(F"{sorted(family)} temp sorting the list with sorted")
+
+family.reverse()
+print(f"{family} now lets flip it with the reverse this is the original list being fliped ")
+
+print(f"{family[-1]} <- so if you say [-1] this is the last element in the list/array [start : end] is how you can think about it [3:5] so this starts at index 1 then goes to index 5")
+print("works alot like a loop just syntax different  ")
---
+++ book/chapter_3/forloop.py
@@ -0,0 +1,11 @@
+pizzas = ["cheese","pepperoni","meat lovers"]
+
+for pie in pizzas:
+    print(f" I love {pie} pizza : you can do it this way becuase python is cool -> for pie in pizzas")
+
+for i in range(len(pizzas)):
+    print(f" I like {pizzas[i]} pizza : i did this with index on the list -> for i in range(len(pizzas))")
+
+print("these are my top 3 pizzas")
+
+print("\n")
