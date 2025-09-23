d200b2dc74b54f09b2bdc581320d9f24cfa8387f
Bryruto <114258995+Bryruto@users.noreply.github.com>
2025-09-23 09:29:21 -0600

    k

--- 1410/out/file/files.py
+++ 1410/out/file/files.py
@@ -1,17 +1,8 @@
-import csv
-import pandas
 def main():
-    with open("fileiOData","r") as file:
-        print("this is fileiOData")
+    filename=input()
+    with open(filename,"r") as file:
+        counter = 0
         for line in file:
-            print(line)
-    with open("fileiOData2","r") as file:
-        print("this is fileiOData2")
-        for line in file:
-            print(line)
-    with open("people.txt","r") as file:
-        print("this is people.txt")
-        for line in file:
-            print(line)
-
+            counter +=1
+            print(f"this is line {counter}:{line}")       
 main()
