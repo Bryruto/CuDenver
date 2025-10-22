d1561dcef228f3884cd5f8e1327ad0324706396b
Bryruto <114258995+Bryruto@users.noreply.github.com>
2025-10-22 13:31:35 -0600

    he

---
+++ 1410/lab/lab9/AndersonLab9a.py
@@ -0,0 +1,45 @@
+# Name:  Brycen Anderson
+
+# Class: CSCI 1411-003
+
+# Due Date: 10/21/2025
+
+# Description: This program how to use functions in a Python
+# program. It also shows how to use parameters to pass data
+# to a function and return data from a function using a return
+# statement.
+
+# Status: Runs as expected.
+
+# f_to_c function
+# parameter: Temperature in degree Fahrenheit (fahrenheit)
+# returns equivalent temperature in degree Celsius
+def f_to_c(fahrenheit):
+    result = (fahrenheit - 32) * (5.0 / 9.0)
+    return result
+
+# f_to_c_list function
+# parameter: a list of temperature in degree Fahrenheit(temp_list)
+# It coverts the temperatures in list to equivalent temperatures in
+# Celsius. There is no return statement as data is returned using
+# the list that is passed as an argument.
+def f_to_c_list(temp_list):
+    for i in range(len(temp_list)):
+        fahrenheit = temp_list[i]
+        celsius = f_to_c (fahrenheit)
+        temp_list[i] = round(celsius,2)
+
+# main function
+def main():  
+    temp_list = []
+    # Ask user for a set of 5 temperature in degree Fahrenheit
+    for i in range(5):
+        fahrenheit = float(input("Enter temperature in degree Fahrenheit: "))
+        temp_list.append(fahrenheit)
+
+    # call the function f_to_c_list
+    f_to_c_list(temp_list)
+    print("The converted temperature list")
+    print(temp_list)
+
+main()
---
+++ 1410/lab/lab9/AndersonLab9b.py
@@ -0,0 +1,48 @@
+# Name:  Brycen Anderson
+
+# Class: CSCI 1411-003
+
+# Due Date: 10/21/2025
+
+# Description: This is of lab 9
+#ask for date (mm/dd/yyyy) then give it back in long fomat
+
+# Status: Runs as expected.
+def month_to_name(number:str)->str:
+   # month_names = {
+    #"01": "January",
+#    "02": "February",
+ #   "03": "March",
+  #  "04": "April",
+   # "05": "May",
+   # "06": "June",
+ #   "07": "July",
+  #  "08": "August",
+   # "09": "September",
+   # "10": "October",
+   # "11": "November",
+   # "12": "December",
+#}
+    months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    try:
+        return months[int(number)-1]
+    except:
+        print(int(number))
+        return "not here"
+   # if number in month_names:
+    #    return month_names[number]
+    #return "not here"
+
+    
+def split_date(date:str)->list:
+    return date.split("/") 
+
+def date_convert(date:str)->str:
+    month,day,year = split_date(date)
+    month = month_to_name(month)
+
+    return f"{date} in long fomat is {month} {day}, {year}"
+def main():
+    date = date_convert(input("Enter date:"))
+    print(date)
+main()
