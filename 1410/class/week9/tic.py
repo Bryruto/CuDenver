bf8c49451b3c4ba923fdc9252a0b78b91280198a
Bryruto <114258995+Bryruto@users.noreply.github.com>
2025-10-27 22:38:13 -0600

    done

--- 1410/class/week9/tic.py
+++ 1410/class/week9/tic.py
@@ -1,78 +1,109 @@
-"""
-Make a data store -
--global available moves 
--owners of spaces 
--dict mapping so 
--do slices of available moves to make row1 row2 row3
--loop available moves if tuple index 1 if 0 = col1 1 = col2 2 = col3
-
-make variable i used to see if even or odd so we can go back and forth  
-
-loop an unknown number of times till winner or no more moves
-
-    build board
-        use data store owners of spaces
-        iterate size of board vairable r
-            iterate size of board vairable c
-                if r % 3 == 0 
-                    output _
-                if r >= 3 and (c % 3 == )
-                    output |
-                if (r % 2 == 0) and (c % 3 == 1):
-                    if maping[(r,c)]
-                        add owners[maping[(r,c)]]
-                
-                        
-
-    output the board with all marks so far
-   
-    if even 
-        ask for input on a tic-tac-toe board 
-
-        store input in variable called player 1 / player 1 will use x to mark board
-    if odd 
-        ask for input on tic-tac-toe board 
-
-        store the input in variable called player 2 will use o to mark board 
-
-    check if that that makes 3 in a row 
-
-            loop through 3 row possibilities 
-
-                check to see if the same player owns them
-
-    check if that that makes 3 in a column
-            loop through 3 column possibilities 
-
-                check to see if the same player owns them 
-
-    check if that makes 2 In a diagonal
-            loop through 2 diagonal possibilities 
-
-                check to see if the same player owns them 
-
-"""
-'''
-Make a data store -
--global available moves 
--owners of spaces 
--dict mapping so 
--do slices of available moves to make row1 row2 row3
--loop available moves if tuple index 1 if 0 = col1 1 = col2 2 = col3
-def is_won(dict):
-'''
+import sys 
 open_moves = [(r,c) for r in range(3) for c in range(3)]
-owners = {}
-mapping ={(r,c):open_moves
-          for r in range(9) for c in range(9) if (r % 3 == 2 and r >=2) and c % 3 == 1
-          }
+mapping = {real:easy for real,easy in zip([(r,c) for r in range(1,10,4) for c in range(1,10,4)] , [(r,c) for r in range(3) for c in range(3)])}#i love zip 
+owns = {}
 
-math = tuple((r,c) for r in range(9) for c in range(9) if (r % 3 == 2 and r >=2) and c % 3 == 1)
 
-print(math)
+#print(mapping)
+
+o = {"row0":0,"row1":0,"row2":0,
+     "col0":0,"col1":0,"col2":0,
+     "d1":0,"d2":0
+     }
+
+x = {"row0":0,"row1":0,"row2":0,
+     "col0":0,"col1":0,"col2":0,
+     "d1":0,"d2":0
+     }
+ 
+def build_a_board():#there must be a better way no time tho
+    for i in range(11):
+        for y in range(11):
+            if i == 3 or i == 7:
+                print("# ", end="")
+            elif y == 3 or y == 7:
+                print("# ", end="")
+            else:
+                if (i, y) in mapping:
+                    easy = mapping[(i, y)]
+                    if easy in owns:
+                        print(owns[easy], end=" ")  
+                    else:
+                        print("  ", end="")                    
+                else:
+                    print("  ", end="")             
+        print()
+
+
+def is_winner(i:int):#check if winner 
+    if i == 0:
+        for v in x.values():
+            build_a_board()
+            if v == 3:
+                return "winner is x"
+        
+    else:
+        for v in o.values():
+            if v == 3:
+                build_a_board()
+                return "winner is o"
+    return ""
 
 
 
 def main():
-    pass
+    winner = ""
+    i = 0
+    print("Tic-Tac-Toe enter 0-2,0-2 ex 2,2 or 2,1 or 1,1")
+    while not winner:
+        build_a_board()
+        if not open_moves:
+            sys.exit("no winners")
+
+        if i == 0:
+            p1 = input("player 1:")
+            if p1 == "stop":#this is for me :\
+                break
+            s1,s2 = p1.split(",")
+            p1 = (int(s1),int(s2))
+
+            if p1 in open_moves:
+                owns[p1] = "x"
+                open_moves.remove(p1)
+                x["row"+str(p1[0])] += 1
+                x["col"+str(p1[1])] += 1
+                
+                if p1[0] == p1[1]:
+                    x["d1"] += 1              
+                if p1[0] + p1[1] == 2:
+                    x["d2"] += 1              
+
+                winner = is_winner(i)
+                i += 1
+            else:
+                print("bad move")#didnt want to do a loop/// its still a loop read to see why 
+
+        else:
+            p2 = input("player 2:")
+            if p2 == "stop":
+                break
+
+            s1,s2 = p2.split(",")
+            p2 = (int(s1),int(s2))
+            if p2 in open_moves:
+                owns[p2] = "o"
+                open_moves.remove(p2)
+                o["row"+str(p2[0])] +=1
+                o["col"+str(p2[1])] += 1
+
+                if p2[0] == p2[1]:
+                    o["d1"] += 1
+                if p2[0] + p2[1] == 2:
+                    o["d2"] += 1
+
+                winner = is_winner(i)
+                i-= 1
+            else:
+                print("bad move ")
+    print(winner)
 main()
